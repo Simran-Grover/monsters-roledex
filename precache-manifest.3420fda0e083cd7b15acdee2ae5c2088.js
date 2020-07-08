13e92d95a442b8c1e1f905e14a337a4cbcba1901
simran-grover <simran.grover@bauschhealth.com>
2020-07-08 15:06:01 +0530

    Updates

---
+++ precache-manifest.3420fda0e083cd7b15acdee2ae5c2088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3bc426672c20c10e7450c1faec8754",
+    "url": "/react-monsters-app/index.html"
+  },
+  {
+    "revision": "b18e6fbf056907e299c9",
+    "url": "/react-monsters-app/static/css/main.72b1ed0d.chunk.css"
+  },
+  {
+    "revision": "6952c745d55c5a0fa7de",
+    "url": "/react-monsters-app/static/js/2.e47ca8d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-app/static/js/2.e47ca8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18e6fbf056907e299c9",
+    "url": "/react-monsters-app/static/js/main.f23d339f.chunk.js"
+  },
+  {
+    "revision": "be1ba786420dcad56136",
+    "url": "/react-monsters-app/static/js/runtime-main.691406f0.js"
+  }
+]);
